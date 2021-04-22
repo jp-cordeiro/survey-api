bc458f6ab0cf4a14b50794de995299ee402b4ad2
JP <jpc.araujo@hotmail.com>
2021-04-22 00:15:52 -0300

    chore: refactoring to use alias path in tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/__tests__'],
   collectCoverageFrom: ['<rooDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
