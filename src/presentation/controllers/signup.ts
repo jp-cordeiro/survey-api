020743ffca5d35cfba2c97668b83e646d132b779
JP <jpc.araujo@hotmail.com>
2021-05-01 18:37:30 -0300

    chore: change pattern of code base

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -11,7 +11,12 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation'
+    ]
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
