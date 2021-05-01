3ff393cdbe987dabf18a5a414932073960381db7
JP <jpc.araujo@hotmail.com>
2021-05-01 20:32:46 -0300

    chore: change pattern of code base

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,17 +6,12 @@ import { EmailValidator } from '@/presentation/protocols/email-validator'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {
+  constructor(private readonly emailValidator: EmailValidator) {
     this.emailValidator = emailValidator
   }
 
-  handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation'
-    ]
+  handle(httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
