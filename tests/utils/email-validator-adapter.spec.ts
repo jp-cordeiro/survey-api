7e60908e96b21bf2a9da6fab367872617c4a25a7
JP <jpc.araujo@hotmail.com>
2021-07-12 23:22:14 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ tests/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from '@/utils/email-validator-adapter'
+
+describe('EmailValidator Adapter', () => {
+  test('should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBeFalsy()
+  })
+})
