9704925fee9c467eaa5b77b8ed2b1f8fa6bb81e8
JP <jpc.araujo@hotmail.com>
2021-09-07 13:32:38 -0300

    test: ensure BcryptAdapter retunr a hash on success

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -12,11 +12,22 @@ const makeSut = (): SutTypes => {
   }
 }
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return await new Promise(resolve => resolve('hash'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt with correct value', async () => {
     const { sut } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', SALT)
+  })
+  test('should call bcrypt with correct value', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
