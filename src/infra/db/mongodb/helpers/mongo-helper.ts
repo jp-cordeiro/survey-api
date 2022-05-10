98bb7c2ffc5ede960328a642002d879becb4fbb3
jp-cordeiro <jpc.araujo@hotmail.com>
2022-05-09 22:26:43 -0300

    refactor: add helper method in MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
-import { AccountModel } from '@/domain/models/account'
-import { Collection, MongoClient } from 'mongodb'
+import { Collection, MongoClient, Document } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -18,8 +17,16 @@ export const MongoHelper = {
     }
     return this.client.db().collection(name)
   },
-  map(account: any): AccountModel {
-    const { _id, ...accountData } = account
-    return { id: _id, ...accountData }
+  mapData(result: Document): any {
+    const data = { id: result._id.toHexString(), ...result }
+    return data
+  },
+  async allDataByCollection(collection: Collection<Document>, result: Document): Promise<any> {
+    const dataResult = await collection.findOne({
+      _id: result.insertedId.toHexString
+    })
+    const { _id, ...rest } = dataResult
+    const data = { id: _id, ...rest }
+    return data
   }
 }
