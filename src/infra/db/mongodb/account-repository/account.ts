22768ed513bf2c9ea649d7ad61ce7f268731f626
jp-cordeiro <jpc.araujo@hotmail.com>
2022-05-23 22:42:05 -0300

    feat: ensure AccountMongoRepository updates account accessToken on updateAccessToken sucess

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,24 +1,35 @@
-import { LoadAccountByEmailRepository } from '@/data/protocols/db'
+import { LoadAccountByEmailRepository, UpdateAccessTokenRepository } from '@/data/protocols/db'
 import { AddAccountRepository } from '@/data/protocols/db/add-account-repository'
 import { AccountModel } from '@/domain/models/account'
 import { AddAccountModel } from '@/domain/useCases/add-account'
+import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = await MongoHelper.allDataByCollection(accountCollection, result)
     return account
   }
 
   async loadByEmail(email: string): Promise<AccountModel | null> {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.findOne({ email })
     if (!result) {
       return null
     }
     const account = await MongoHelper.mapData(result)
     return account
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $set: { accessToken: token }
+      }
+    )
   }
 }
