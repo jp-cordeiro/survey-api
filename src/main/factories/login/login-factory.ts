c1749290f6344ac9c15024a7acb48c64be900d6b
jp-cordeiro <jpc.araujo@hotmail.com>
2022-06-17 21:13:50 -0300

    feat: add LoginFactory

---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,20 @@
+import { DbAuthetication } from '@/data/usecases/db-authentication'
+import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter'
+import { JwtAdapter } from '@/infra/criptography/jwt-adapter'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
+import { LogMongoRepository } from '@/infra/db/mongodb/log-repository/log-repository'
+import { LogControllerDecorator } from '@/main/decorators/log'
+import { LoginController } from '@/presentation/controllers/login/login'
+import { Controller } from '@/presentation/protocols'
+import { makeLoginValidation } from './login-validation-factory'
+
+export const makeLoginController = (): Controller => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter('')
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAuthetication = new DbAuthetication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const loginController = new LoginController(dbAuthetication, makeLoginValidation())
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(loginController, logMongoRepository)
+}
